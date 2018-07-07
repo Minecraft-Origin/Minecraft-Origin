bfd7ead533e7cca314d71d26c7dc28fb87b5b824
Zhang-Wei-666 <994039348@qq.com>
2018-07-08 01:40:36 +0800

    忽略小地图改动

--- git assume-unchanged.bat
+++ git assume-unchanged.bat
@@ -1,2 +1,3 @@
 git update-index --assume-unchanged "Minecraft Origin/hmcl.json"
 git update-index --assume-unchanged "Minecraft Origin/.minecraft/config/InvTweaks.cfg"
+git update-index --assume-unchanged "Minecraft Origin/.minecraft/mods/VoxelMods/voxelmap.properties"
--- git no-assume-unchanged.bat
+++ git no-assume-unchanged.bat
@@ -1,2 +1,3 @@
 git update-index --no-assume-unchanged "Minecraft Origin/hmcl.json"
 git update-index --no-assume-unchanged "Minecraft Origin/.minecraft/config/InvTweaks.cfg"
+git update-index --no-assume-unchanged "Minecraft Origin/.minecraft/mods/VoxelMods/voxelmap.properties"
