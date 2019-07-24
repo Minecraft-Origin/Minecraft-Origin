1be4ea9e8395bd1e23c5d2270f4cbbf8a990e13e
Zhang-Wei-666 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-07-25 03:44:20 +0800

    Update git assume-unchanged.bat

--- git assume-unchanged.bat
+++ git assume-unchanged.bat
@@ -1,4 +1,3 @@
 git update-index --assume-unchanged "Minecraft Origin/hmcl.json"
 git update-index --assume-unchanged "Minecraft Origin/.minecraft/config/InvTweaks.cfg"
-git update-index --assume-unchanged "Minecraft Origin/.minecraft/config/veinminer/tools-and-blocks.json"
 git update-index --assume-unchanged "Minecraft Origin/.minecraft/liteconfig/common/autofish.json"
