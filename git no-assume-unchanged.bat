312827e7c0d9935296e2a86d6694049013d6baa0
Zhang-Wei-666 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-07-28 04:09:52 +0800

    Update git no-assume-unchanged.bat

--- git no-assume-unchanged.bat
+++ git no-assume-unchanged.bat
@@ -1,3 +1,4 @@
 git update-index --no-assume-unchanged "Minecraft Origin/hmcl.json"
 git update-index --no-assume-unchanged "Minecraft Origin/.minecraft/config/InvTweaks.cfg"
 git update-index --no-assume-unchanged "Minecraft Origin/.minecraft/liteconfig/common/autofish.json"
+git update-index --assume-unchanged "Minecraft Origin/.minecraft/mods/VoxelMods/voxelmap.properties"
