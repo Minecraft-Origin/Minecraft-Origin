6dc0214dff51fd4e19e923614f1b3f13fbbd74e4
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2020-10-01 21:36:18 +0800

    Update git assume-unchanged.bat

--- git assume-unchanged.bat
+++ git assume-unchanged.bat
@@ -1,5 +1,6 @@
 git update-index --assume-unchanged "Minecraft Origin/hmcl.json"
 git update-index --assume-unchanged "Minecraft Origin/.minecraft/config/InvTweaks.cfg"
+git update-index --assume-unchanged "Minecraft Origin/.minecraft/config/InvTweaks/InvTweaks.cfg"
 git update-index --assume-unchanged "Minecraft Origin/.minecraft/config/randomtweaks/data.json"
 git update-index --assume-unchanged "Minecraft Origin/.minecraft/journeymap/config/5.7/journeymap.core.config"
 git update-index --assume-unchanged "Minecraft Origin/.minecraft/journeymap/config/5.7/journeymap.fullmap.config"
